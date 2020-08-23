a63730b0001348e81e0825a63f334ad57880ce78
Luna <git@l4.pm>
2020-08-23 13:16:25 -0300

    initialize libsoundio

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
+const soundio = @import("soundio.zig");
 
-pub fn main() anyerror!void {
+pub fn main() anyerror!u8 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer {
         _ = gpa.deinit();
@@ -19,4 +20,15 @@ pub fn main() anyerror!void {
     const path = try (args_it.next(allocator) orelse @panic("expected path arg"));
     defer allocator.free(path);
     std.debug.warn("path: {}\n", .{path});
+
+    var soundio_opt = soundio.c.soundio_create();
+    if (soundio_opt == null) {
+        std.debug.warn("libsoundio fail: out of memory\n", .{});
+        return 1;
+    }
+
+    var soundio_state = soundio_opt.?;
+    defer soundio.c.soundio_destroy(soundio_state);
+    std.debug.warn("initialized libsoundio\n", .{});
+    return 0;
 }
