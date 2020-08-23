f3c9a5617214c95dc3ac632553f1456b79acb393
Luna <git@l4.pm>
2020-08-23 12:51:15 -0300

    add arg fetching code

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,19 @@
 const std = @import("std");
 
 pub fn main() anyerror!void {
-    std.debug.warn("All your codebase are belong to us.\n", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        _ = gpa.deinit();
+    }
+    var allocator = &gpa.allocator;
+
+    var args_it = std.process.args();
+    defer args_it.deinit();
+
+    std.debug.assert(args_it.skip() == true);
+
+    const url = try (args_it.next(allocator) orelse @panic("expected url arg"));
+    defer allocator.free(url);
+
+    std.debug.warn("url: {}\n", .{url});
 }
